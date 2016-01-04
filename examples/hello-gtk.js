7c1721e998ddbbf92843205ce1783e29bac28904
Ben Iofel <iofelben+github@gmail.com>
2016-01-04 03:40:55 -0500

    Use 'on' instead of 'connect' for signals

--- examples/hello-gtk.js
+++ examples/hello-gtk.js
@@ -16,8 +16,8 @@ win = new Gtk.Window({
   window_position: Gtk.WindowPosition.CENTER
 });
 
-win.connect('show', Gtk.main);
-win.connect('destroy', Gtk.main_quit);
+win.on('show', Gtk.main);
+win.on('destroy', Gtk.main_quit);
 
 win.set_default_size(200, 80);
 win.add(new Gtk.Label({label: 'Hello Gtk+'}));
