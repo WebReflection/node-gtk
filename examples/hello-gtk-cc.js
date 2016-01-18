c66ab146deb4c7a56aed70e2d7c73ff2d05fa0e4
webreflection <andrea.giammarchi@gmail.com>
2016-01-18 21:28:21 +0000

    Using withCamelCase method to pollute namespaces

---
+++ examples/hello-gtk-cc.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+var
+  GNode = require('../lib/'),
+  Gtk = GNode.withCamelCase('Gtk'),
+  settings,
+  win
+;
+
+GNode.startLoop();
+Gtk.init(null);
+
+settings = Gtk.Settings.getDefault(),
+settings.gtkApplicationPreferDarkTheme = true;
+settings.gtkThemeName = "Adwaita";
+
+console.log(settings.gtkEnableAccels);
+
+win = new Gtk.Window({
+  title: 'node-gtk'
+  // __init__ not implemented yet
+  // , window_position: Gtk.WindowPosition.CENTER
+});
+
+win.connect('show', Gtk.main);
+win.connect('destroy', Gtk.mainQuit);
+
+win.setDefaultSize(200, 80);
+win.add(new Gtk.Label({label: 'Hello Gtk+'}));
+
+win.showAll();
